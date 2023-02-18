8cc461cd8a0943bd4574d7d5493e3b813d2ced50
JefvdA <jef.v.d.a@live.be>
2023-02-18 18:54:41 +0100

    fix: added flask-migrate setup

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 
 db = SQLAlchemy()
+migrate = Migrate(app, db)
 db.init_app(app)
 
 from flaskr.entities.Client import Client
