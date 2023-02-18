901b72799228edb5cc7c8e12f2572f0509f06ac5
JefvdA <jef.v.d.a@live.be>
2023-02-18 12:39:43 +0100

    feat: added minimal API structure

---
+++ flaskr/views.py
@@ -0,0 +1,10 @@
+from flaskr import api
+from flask_restful import Resource
+
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+
+api.add_resource(HelloWorld, '/')
