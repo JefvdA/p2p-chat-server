03162c863440ae35a307dad8eb27bf1e7b1e3db7
JefvdA <jef.v.d.a@live.be>
2023-02-18 14:34:43 +0100

    feat: added register endpoint

--- flaskr/views.py
+++ flaskr/views.py
@@ -1,5 +1,8 @@
 from flaskr import api
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
+
+parser = reqparse.RequestParser()
+parser.add_argument('client')
 
 
 class HelloWorld(Resource):
@@ -7,4 +10,20 @@ class HelloWorld(Resource):
         return {'hello': 'world'}
 
 
+class Register(Resource):
+    def post(self):
+        args = parser.parse_args()
+        client = args['client']
+
+        if 'username' not in client:
+            return {"message": "The client provided can't be registered! A client requires a username"}
+        if 'host' not in client:
+            return {"message": "The client provided can't be registered! A client requires a host"}
+
+        print(client)
+
+        return {"message": "ok"}, 201
+
+
 api.add_resource(HelloWorld, '/')
+api.add_resource(Register, '/register')
